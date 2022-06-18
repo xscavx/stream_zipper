50d57ddca0bf3047cb35787e187072eb3592746b
xscav <ur.pekax@gmail.com>
2022-06-18 21:39:49 +0300

    Implemented overflow, sync, buffer pointer arithmetics.

--- include/stream_zip/deflate_streambuf.hpp
+++ include/stream_zip/deflate_streambuf.hpp
@@ -3,46 +3,23 @@
 #include <zlib.h>
 
 
-inline constexpr auto CHUNK_SIZE = 10;
-
-
-template<int BUFSIZE>
+template<size_t IN_BUFFER_SIZE, size_t OUT_BUFFER_SIZE>
 class zipbuf : public std::streambuf {
 public:
+   using byte_type = Bytef;
+
    explicit zipbuf(std::ostream &out_stream, int compression_level = Z_DEFAULT_COMPRESSION)
       : out_stream_(out_stream)
    {
-      init_deflate_state(compression_level);
-      init_buffers_state();
-   }
-
-   ~zipbuf() {
-      deflateEnd(&stream_state_);
-   }
-
-   void init_deflate_state(int compression_level) {
-      stream_state_.zalloc = Z_NULL;
-      stream_state_.zfree = Z_NULL;
-      stream_state_.opaque = Z_NULL;
-      deflateInit(&stream_state_, compression_level);
-   }
-
-   void init_buffers_state() {
-      stream_state_.next_out = reinterpret_cast<Bytef*>(out_buffer_.data());
-      stream_state_.avail_out = static_cast<uInt>(out_buffer_.size());
-      
-      auto out_begin = out_buffer_.data();
-      auto out_end = out_begin + static_cast<uInt>(out_buffer_.size());
-      setp(out_begin, out_end);
+      deflate_init(deflate_state_, compression_level);
+      out_buffer_prepare_for_deflate(deflate_state_);
+      in_buffer_prepare_for_input();
    }
 
    int_type overflow(int_type c) override {
       if (pptr() > pbase()) {
-         if (!zip_buffer())
+         if (!input_buffer_deflate(Z_NO_FLUSH))
             return traits_type::eof();
-
-         // pptr() automamtically sets to pbase()
-         setp(pbase(), epptr());
       }
 
       // write c if not EOF
@@ -61,21 +38,82 @@ public:
       return 0;
    }
 
-   bool zip_buffer() {
-      stream_state_.next_in = reinterpret_cast<Bytef*>(pbase());
-      // input buffer available bytes
-      stream_state_.avail_in =  (pptr() - pbase()) * sizeof(char_type);
+   void deflate_init(z_stream &stream, int compression_level) {
+      stream.zalloc = Z_NULL;
+      stream.zfree = Z_NULL;
+      stream.opaque = Z_NULL;
+      deflateInit(&stream, compression_level);
+   }
 
-      int deflate_state{Z_OK};
-      while (stream_state_.avail_in > 0 && deflate_state == Z_OK) {
-         deflate_state = deflate(&stream_state_, Z_NO_FLUSH);
+   bool input_buffer_deflate(int flush) {
+      in_buffer_prepare_for_deflate(deflate_state_);
+      int last_deflate_status_code{Z_OK};
+
+      while (deflate_state_.avail_in > 0 && last_deflate_status_code == Z_OK) {
+         last_deflate_status_code = deflate(&deflate_state_, flush);
+
+         if (last_deflate_status_code == Z_OK) {
+            bool const out_buffer_overflow = deflate_state_.avail_out == 0;
+            if (out_buffer_overflow) {
+               out_buffer_write_to_ostream();
+               out_buffer_prepare_for_deflate(deflate_state_);
+            }
+         }
       }
+
+      bool const out_buffer_has_data = (
+            OUT_BUFFER_BYTES - deflate_state_.avail_out) > 0;
+      if (out_buffer_has_data) {
+         out_buffer_write_to_ostream();
+         out_buffer_prepare_for_deflate(deflate_state_);
+      }
+
+      in_buffer_prepare_for_input();
       return true;
    }
 
+   void in_buffer_prepare_for_input() {
+      auto const in_begin = in_buffer_.data();
+      setp(in_begin, in_begin + in_buffer_.size());
+   }
+
+   void in_buffer_prepare_for_deflate(z_stream &stream) {
+      stream.next_in = reinterpret_cast<byte_type*>(pbase());
+      stream.avail_in = static_cast<uInt>((pptr() - pbase()) * CHAR_TYPE_SIZE);
+   }
+
+   void out_buffer_prepare_for_deflate(z_stream &stream) {
+      auto [full_chars, remainder] = std::lldiv(
+         OUT_BUFFER_BYTES - deflate_state_.avail_out,
+         CHAR_TYPE_SIZE
+      );
+      // copy not full char of last deflate to begin of buffer
+      if (remainder) {
+         out_buffer_[0] = out_buffer_[full_chars];
+      }
+      stream.next_out = reinterpret_cast<byte_type*>(out_buffer_.data()) + remainder;
+      stream.avail_out = static_cast<uInt>(OUT_BUFFER_BYTES);
+   }
+
+   void out_buffer_write_to_ostream() {
+      auto const last_deflate_whole_chars_count = static_cast<std::streamsize>(
+            (OUT_BUFFER_BYTES - deflate_state_.avail_out) / CHAR_TYPE_SIZE);
+      out_stream_.write(out_buffer_.data(), last_deflate_whole_chars_count);
+   }
+
+   void zflush() {
+      sync();
+      input_buffer_deflate(Z_FINISH);
+      out_stream_.flush();
+	   deflateEnd(&deflate_state_);
+   }
+
 public:
-   std::array<char_type, BUFSIZE> in_buffer_{}, out_buffer_{};
-   z_stream stream_state_{};
+   std::array<char_type, IN_BUFFER_SIZE> in_buffer_{};
+   std::array<char_type, OUT_BUFFER_SIZE> out_buffer_{};
+   z_stream deflate_state_{};
    std::ostream &out_stream_;
-   std::istream *in_stream_;
+   static constexpr auto CHAR_TYPE_SIZE{sizeof(char_type)};
+   static constexpr auto IN_BUFFER_BYTES = IN_BUFFER_SIZE * CHAR_TYPE_SIZE;
+   static constexpr auto OUT_BUFFER_BYTES = OUT_BUFFER_SIZE * CHAR_TYPE_SIZE;
 };
--- tests/deflate_streambuf_test.cpp
+++ tests/deflate_streambuf_test.cpp
@@ -1,14 +1,28 @@
 #define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-#include <stream_zip/deflate_streambuf.hpp>
 
 #include <iostream>
 #include <string>
+#include <catch2/catch.hpp>
+#include <stream_zip/deflate_streambuf.hpp>
 
 
-TEST_CASE( "Quick check", "[main]" ) {
-  std::string string_in{"test string very large wowow, stp it please, please please test string very large wowow, stp it please, please please"};
-  std::istringstream stringstream_in{string_in};
-  zipbuf<CHUNK_SIZE> zbuf{std::cout};
-  REQUIRE(true);
+TEST_CASE( "Zipper stream testing", "[main]" ) {
+  static constexpr std::string_view string_in{"test string: zip me please"};
+  static constexpr auto iterations = 30000;
+  static constexpr auto in_buf_size = string_in.size() * iterations;
+
+  std::cout << "source string:\n" << string_in << std::endl;
+  std::cout << "final string:\n";
+
+  {
+    zipbuf<300000, 600000> zbuf{std::cout, Z_NO_COMPRESSION};
+    std::ostream zip_output(&zbuf);
+    for (int i = 0; i < iterations; ++i) {
+      zip_output << string_in << " ";
+    }
+    zbuf.zflush();
+  }
+
+  std::cout << std::endl;
+  REQUIRE(false);
 }
